7b4b9cb98446eaa1013757f2fb01defe19346605
MY PC <juvvalapalpeuprameela879@gmail.com>
2024-06-21 22:29:57 +0530

    second commit

---
+++ lab009.py
@@ -0,0 +1,26 @@
+list=['asdf','fgh','poinm']
+
+print(list)
+
+list.insert(3,10089)
+print(list)
+list2=['890','789','456']
+list.extend(list2)
+list3=['345','456',678]
+list.extend(list3)
+print(list)
+list=[1,2,3,4,5]
+print(sum(list))
+print(list.count(1))
+print(list.count(2))
+print(len(list))
+print(min(list))
+print(max(list))
+print(list.sort())
+print(list)
+print(list.reverse())
+print(list)
+print(list.pop(0))
+del list[3]
+print(list)
+
